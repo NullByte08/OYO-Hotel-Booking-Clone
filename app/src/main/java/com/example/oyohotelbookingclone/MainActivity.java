019d4507da5ead79e28e6e96bcae6ebf355f6064
Cheenu <cheenusoni10@gmail.com>
2019-10-09 22:05:22 +0530

    commit 1

--- app/src/main/java/com/example/oyohotelbookingclone/MainActivity.java
+++ app/src/main/java/com/example/oyohotelbookingclone/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.oyohotelbookingclone;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button searchbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        searchbar=findViewById(R.id.searchBar);
+        searchbar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment fragment=new HomeFragment();
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer,fragment).commit();
+            }
+        });
     }
 }
